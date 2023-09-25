4d3b67174e725d8289dda7b12d13d29199eca00e
TOM4S <tomyamigos@gmail.com>
2023-09-25 20:10:23 -0300

    AlertLog Service

---
+++ API.PastillApp.Services/Services/AlertLogService.cs
@@ -0,0 +1,101 @@
+﻿using API.PastillApp.Domain.Entities;
+using API.PastillApp.Repositories.Interface;
+using API.PastillApp.Services.DTOs;
+using API.PastillApp.Services.Interfaces;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.PastillApp.Services.Services
+{
+    public class AlertLogService : IAlertLogService
+    {
+        private readonly IAlertLogRepository _alertLogRepository;
+        private readonly IMapper _mapper;
+
+        public AlertLogService(IAlertLogRepository alertLogRepository, IMapper mapper)
+        {
+            _alertLogRepository = alertLogRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<AlertLog> GetAlertLog(int alertLogId)
+        {
+            return await _alertLogRepository.GetAlertLogById(alertLogId);
+        }
+
+        public async Task<List<AlertLog>> GetAllAlertLogs()
+        {
+            return await _alertLogRepository.GetAllAlertLogs();
+        }
+
+        public async Task<ResponseDTO> CreateAlertLog(CreateAlertLogDTO alertLogDTO)
+        {
+            try
+            {
+                var alertLog = _mapper.Map<AlertLog>(alertLogDTO);
+
+                // Agregar el nuevo registro de AlertLog a la base de datos
+                await _alertLogRepository.AddAlertLog(alertLog);
+
+                // Crear y devolver una respuesta de éxito
+                var response = new ResponseDTO
+                {
+                    isSuccess = true,
+                    message = "Registro de AlertLog creado con éxito",
+                };
+
+                return response;
+            }
+            catch (Exception ex)
+            {
+                // En caso de error, crear y devolver una respuesta de error
+                var errorResponse = new ResponseDTO
+                {
+                    isSuccess = false,
+                    message = "Error al crear el registro de AlertLog",
+                };
+
+                return errorResponse;
+            }
+        }
+
+        public async Task<ResponseDTO> UpdateAlertLog(AlertLog alertLog)
+        {
+            try
+            {
+                await _alertLogRepository.UpdateAlertLog(alertLog);
+                return new ResponseDTO { isSuccess = true };
+            }
+            catch (Exception ex)
+            {
+                // Log the error or handle it as needed
+                Console.WriteLine($"Error al actualizar el registro de AlertLog: {ex.Message}");
+                return new ResponseDTO { isSuccess = false, message = "Error al actualizar el registro de AlertLog" };
+            }
+        }
+
+        public async Task<ResponseDTO> DeleteAlertLog(int alertLogId)
+        {
+            try
+            {
+                await _alertLogRepository.DeleteAlertLog(alertLogId);
+                return new ResponseDTO { isSuccess = true };
+            }
+            catch (Exception ex)
+            {
+                // Log the error or handle it as needed
+                Console.WriteLine($"Error al eliminar el registro de AlertLog: {ex.Message}");
+                return new ResponseDTO { isSuccess = false, message = "Error al eliminar el registro de AlertLog" };
+            }
+        }
+
+        public Task<List<AlertLog>> GetAllAlertLogsByUserId(int userId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ API.PastillApp/Controllers/AlertLogController.cs
@@ -0,0 +1,47 @@
+﻿using API.PastillApp.Services.DTOs;
+using API.PastillApp.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace API.PastillApp.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AlertLogController : ControllerBase
+    {
+        private readonly IAlertLogService _alertLogService;
+
+        public AlertLogController(IAlertLogService alertLogService)
+        {
+            _alertLogService = alertLogService;
+        }
+
+        [HttpGet("{alertLogId}")]
+        public async Task<IActionResult> GetAlertLog(int alertLogId)
+        {
+            var result = await _alertLogService.GetAlertLog(alertLogId);
+            if (result != null)
+            {
+                return Ok(result);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateAlertLog(CreateAlertLogDTO createAlertLogDTO)
+        {
+            var result = await _alertLogService.CreateAlertLog(createAlertLogDTO);
+            if (result.isSuccess)
+            {
+                return Ok();
+            }
+            else
+            {
+                return BadRequest(result);
+            }
+        }
+    }
+}
