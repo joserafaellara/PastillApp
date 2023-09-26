294e9c8c23d8ababb7fe5d39cb1575fb52dacc66
TOM4S <tomyamigos@gmail.com>
2023-09-25 22:29:20 -0300

    mapping

--- API.PastillApp.Services/Mapper/MappingProfile.cs
+++ API.PastillApp.Services/Mapper/MappingProfile.cs
@@ -26,6 +26,21 @@ namespace API.PastillApp.Services.Mapper
                 .ForMember(dest => dest.Date, opt => opt.MapFrom(src => src.Date))
                 .ForMember(dest => dest.Symptoms, opt => opt.MapFrom(src => src.Symptoms));
 
+
+            CreateMap<CreateMedicineDTO, Medicine>()
+                .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
+                .ForMember(dest => dest.Dosage, opt => opt.MapFrom(src => src.Dosage))
+                .ForMember(dest => dest.Presentation, opt => opt.MapFrom(src => src.Presentation));
+
+            CreateMap<CreateReminderDTO, Reminder>()
+                .ForMember(dest => dest.UserId, opt => opt.MapFrom(src => src.UserId))
+                .ForMember(dest => dest.MedicineId, opt => opt.MapFrom(src => src.MedicineId))
+                .ForMember(dest => dest.Quantity, opt => opt.MapFrom(src => src.Quantity))
+                .ForMember(dest => dest.DateTimeStart, opt => opt.MapFrom(src => src.DateTimeStart))
+                .ForMember(dest => dest.Frequency, opt => opt.MapFrom(src => src.Frequency))
+                .ForMember(dest => dest.EmergencyAlert, opt => opt.MapFrom(src => src.EmergencyAlert))
+                .ForMember(dest => dest.Observation, opt => opt.MapFrom(src => src.Observation))
+                .ForMember(dest => dest.FinalDate, opt => opt.MapFrom(src => src.FinalDate));
         }
     }
 }
