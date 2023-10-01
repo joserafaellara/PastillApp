b305d1255026822fee30a7f2384aa1e23e93ab9f
camilaszesko <camilaszesko2001@gmail.com>
2023-09-30 21:24:44 -0300

    creacion enums para reminders

---
+++ API.PastillApp.Domain/Enums/IntakeFrecuency.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.PastillApp.Domain.Enums
+{
+    public enum IntakeFrecuency
+    {
+        Diario,
+        Semanal,
+        Quincenal,
+        Mensual
+    }
+}
