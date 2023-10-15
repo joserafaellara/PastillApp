84260576b6e073d087978af64d1c050ff3970aef
camilaszesko <camilaszesko2001@gmail.com>
2023-10-14 22:52:54 -0300

    adecuacion para update reminder

---
+++ API.PastillApp.Services/DTOs/UpdateReminderDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.PastillApp.Services.DTOs
+{
+    public class UpdateReminderDTO
+    {
+        public int ReminderId { get; set; }
+        public double Quantity { get; set; }
+        public string? Presentation { get; set; }
+        public DateTime DateTimeStart { get; set; }
+        public int FrequencyNumber { get; set; }
+        public string FrequencyText { get; set; }
+        public int IntakeTimeNumber { get; set; }
+        public string IntakeTimeText { get; set; }
+    }
+}
